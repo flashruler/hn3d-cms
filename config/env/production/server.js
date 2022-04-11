54da2b4d1419521c51b68c8c6418efb8683bced1
flashruler <44584657+flashruler@users.noreply.github.com>
2022-04-11 15:13:14 -0700

    update

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: process.env.HOST || "0.0.0.0",
-  port: process.env.PORT,
+  host: "0.0.0.0",
+  port: `${process.env.PORT}`,
   app: {
     keys: env.array("APP_KEYS"),
   },
